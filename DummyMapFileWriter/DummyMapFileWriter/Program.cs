a44fb187bf62c2c9f1a57e2b8769c01c6f628cf8
CuriousLocky <835322750jm37@outlook.com>
2022-06-01 15:12:40 +0900

    Update dummy test

--- DummyMapFileWriter/DummyMapFileWriter/Program.cs
+++ DummyMapFileWriter/DummyMapFileWriter/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.IO.MemoryMappedFiles;
 using System.Text;
 
-class FileWriter{
-    const int height = 512;
-    const int width = 512;
-    static void Main(String[] args) {
+class FileWriter {
+    const int height = 1080;
+    const int width = 1920;
+    static Semaphore s;
+    static Bitmap midBitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+    static void Main(String[] args)
+    {
         int rgbArraySize = height * width * 3;
-        using(MemoryMappedFile mmf = MemoryMappedFile.CreateOrOpen("PPlusCameraSharedBuffer", rgbArraySize)) {
+        s = new Semaphore(0, 1, "Global\\PPlusVideoFrameSemaphore");
+        using (MemoryMappedFile mmf = MemoryMappedFile.CreateOrOpen("PPlusCameraSharedBuffer", rgbArraySize)) {
             MemoryMappedViewAccessor accessor = mmf.CreateViewAccessor();
             byte[] buffer = new byte[rgbArraySize];
             //accessor.WriteArray(0, buffer, 0, buffer.Length);
@@ -22,50 +27,48 @@ class FileWriter{
         }
     }
 
-    static void parseInput(String input, byte[] buffer){
+    static void parseInput(String input, byte[] buffer)
+    {
         if ((input == null) || (input.Length == 0)) {
             return;
         } else if (input.Equals("blank")) {
             blank(buffer);
-        } else if (input.Equals("q")){
+        } else if (input.Equals("q")) {
             Environment.Exit(0);
         } else {
             readRGBArray(input, buffer);
         }
     }
 
-    static void blank(byte[] buffer){
-        for (int i = 0; i < buffer.Length; i++) {
-            buffer[i] = 0;
+    static void blank(byte[] buffer)
+    {
+        s.WaitOne(0);
+        using (Graphics g = Graphics.FromImage(midBitmap)) {
+            g.Clear(Color.White);
         }
+        s.Release(1);
     }
 
-    static void readRGBArray(String filePath, byte[] buffer) {
-        using(Bitmap bitmap = new Bitmap(filePath)) {
-            int rowSize = Math.Min(bitmap.Width, width);
-            int colSize = Math.Min(bitmap.Height, height);
-        
-            Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+    static void readRGBArray(String filePath, byte[] buffer)
+    {
+        using (Bitmap bitmap = new Bitmap(filePath)) {
+            s.WaitOne(0);
+
+            Rectangle rect = new Rectangle(0, 0, width, height);
+
+            using (Graphics g = Graphics.FromImage(midBitmap)) {
+                g.Clear(Color.White);
+                g.DrawImage(bitmap, Point.Empty);
+            }
+
             System.Drawing.Imaging.BitmapData bitmapData =
-                bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
-                bitmap.PixelFormat);
+                midBitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                PixelFormat.Format24bppRgb);
 
-            IntPtr ptr = bitmapData.Scan0;
+            System.Runtime.InteropServices.Marshal.Copy(bitmapData.Scan0, buffer, 0, width * height * 3);
 
-            int bitmapBytes = Math.Abs(bitmapData.Stride) * bitmap.Height;
-            byte[] rgbValues = new byte[bitmapBytes];
-        
-            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bitmapBytes);
-
-            for (int i = 0; i < buffer.Length; i++) {
-                buffer[i] = 0;
-            }
-
-            for (int row = 0; row < colSize; row++) {
-                Array.Copy(rgbValues, row * rowSize * 3, buffer, row * width * 3, rowSize * 3);
-            }
-
-            bitmap.UnlockBits(bitmapData);
+            midBitmap.UnlockBits(bitmapData);
+            s.Release(1);
         }
     }
 }
